15747837f86c37851c4fd03d27ff46c778595a73
slashedtires <kenneth.m.piekarski@gmail.com>
2013-10-03 14:35:22 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>zine.offtempo</title>
+<link type="text/css" rel="stylesheet" media="all" href="style.css" /> 
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"> 
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-6633559-3']);
+  _gaq.push(['_setDomainName', '.offtempo.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</head>
+
+<body>
+	
+    <div id="container">
+        <div id="offtempo">
+        	<div style="position:absolute; margin-left:-5px; margin-top:0px; font-size:16px; color:#999;"><i>the</i></div>
+        	<img src="/images/offtempo.png" />
+            <div style="margin-left:120px; margin-top:-30px; font-size:16px; color:#999;"><i>zine</i></div>
+        </div>
+        <div id="content">
+        	<div class="print-links">
+            	<div class="publish-date">Published on March, 4 2011</div>
+            	<ul>
+                	<li><a href="/off tempo zine 1.pdf">OFF TEMPO #1</a></li>
+                </ul>
+                <div class="contributors"><strong>Contributors:</strong> Garrett Kelly, Ryan McCormick, Anita Sparrow, Geoff Traeger, Clyde Petersen,  Jonathan Cunningham (DJ Wordlush), Kenneth M Piekarski, Kanako.</div>
+            </div>
+        	<div class="print-links">
+            	<div class="publish-date">Published on July, 9th 2011</div>
+            	<ul>
+                	<li><a href="/off tempo zine 2.pdf">OFF TEMPO #2</a></li>
+                </ul>
+                <div class="contributors"><strong>Contributors:</strong> Garrett Kelly, Rachel LeBlanc, Seth Olinsky, Kenneth M. Piekarski</div>
+            </div>
+            
+        	<div class="print-links">
+            	<div class="publish-date">Published on March, 31st 2012</div>
+            	<ul>
+                	<li><a href="/off tempo zine 3.pdf">OFF TEMPO #3</a></li>
+                </ul>
+                <div class="contributors"><strong>Contributors:</strong> Garrett Kelly, Eli Moore, Phil Elverum, Shelby Turner, Charlie's Zalien, Kenneth M. Piekarski, Geoff Traeger</div>
+            </div>
+            
+            	<p>This particular variation of <em>Off Tempo</em> is a zine featuring contributors of the Pacific Northwestern United States. The contributing writers are those who are part of and are active participants in NW underground music culture. It will be produced in <!--<sup class="frac">1</sup>&frasl;&nbsp;<span class="frac">6</span>--> intermittantly based off of funding for print.</p>
+                <p>This zine is free. Help supporting print and distribution costs by donating what you deem appropriate. Thank you either way: </p>
+                <p>&nbsp;</p>
+                <p align="center"><center><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                <input type="hidden" name="cmd" value="_s-xclick">
+                <input type="hidden" name="hosted_button_id" value="ADUELEFVPU7L4">
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                </form></center></p>
+                <p>&nbsp;</p>
+
+                <p><em>Off Tempo</em> is a multi-media archive project predominantly covering music in Seattle and other music hotbeds of Northwestern Washington. The project is focuses on local and touring musicians performing in alternative venues in all-ages / DIY communities.</p>
+                <p><a href="http://www.offtempo.com/about/">Read all about it</a>.</p>
+                <p>Pick up a physical copy of this publication in...</p>
+                <p style="text-decoration:underline;">Seattle, WA:</p>
+                <p>
+                    <ul>
+                        <li><a href="http://www.myspace.com/healthytimesfunclub" target="_new">Healthy Times Fun Club</a> (Secret)</li>
+                        <li><a href="http://hollowearthradio.org/" target="_new">Hollow Earth Radio</a> (Central District)</li>
+                        <li><a href="http://www.facebook.com/funnybutton" target="_new">Funny Button</a> (University District)</li>
+                        <li><a href="http://www.caferacerseattle.com/" target="_new">Café Racer</a> (University District)</li>
+                        <li><a href="http://maps.google.com/maps?client=safari&oe=UTF-8&ie=UTF8&q=every+day+music+seattle&fb=1&gl=us&hq=every+day+music&hnear=Seattle,+WA&cid=0,0,4564598365781692643&t=h&z=16" target="_new">Every Day Music</a> (Capitol Hill)</li>
+                        <li><a href="http://theveraproject.org/" target="_new">the Vera Project</a> (Belltown / Lower Queen Anne)</li>
+                        <li><a href="http://2020cycle.com/" target="_new">20/20 Cycle</a> (Central District)</li>
+                    </ul>
+                </p>
+                <p style="text-decoration:underline;">Anacortes, WA:</p>
+                <p>
+                	<ul>
+                    	<li><a href="http://thebusinessanacortes.com/" target="_new">The Business</a> (Downtown)</li>
+                    </ul>
+                </p>
+                <p style="text-decoration:underline;">Olympia, WA:</p>
+                <p>
+                	<ul>
+                    	<li><a href="http://maps.google.com/maps?q=phantom+city+records+olympia&hl=en&client=safari&fb=1&gl=us&view=map&cid=4869609965463237834&t=h&z=16&iwloc=A" target="_new">Phantom City Records / Dumpster Values</a> (Downtown)</li>
+                    </ul>
+                </p>
+        </div>
+	</div>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title></title>
+<link type="text/css" rel="stylesheet" media="all" href="style.css" /> 
+
+</head>
+
+<body>
+	
+    <div id="container">
+        <div id="offtempo">
+        </div>
+        <div id="content">
+        	<div class="print-links">
+            	<ul>
+                	<li><a href="/off tempo zine 1.pdf">Off Tempo #1</a></li>
+            		<li><a href="/off tempo zine 1-print.pdf">Off Tempo #1</a> - version to be printed by you: light-weight, light pink card-stock, 8.5" x 11"; print double sided, cut in half vertically</li>
+                </ul>
+            </div>
+                <p>Off Tempo is a multi-media archive project predominantly covering music in Seattle and other music hotbeds of Northwestern Washington. The project is focuses on local and touring musicians performing in alternative venues in all-ages / DIY communities.</p>
+                <p><a href="http://www.offtempo.com/about/">Read all about it</a>.</p>
+        </div>
+	</div>
+</body>
+</html>
